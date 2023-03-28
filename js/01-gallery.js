e2b0b6c39be5a4d64c400236ef89e36dd49ad4af
Sergey_Sanyuk <shv08@ukr.net>
2023-03-28 14:22:42 +0300

    last

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,119 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galaryRef = document.querySelector(".gallery");
+const newGalleryItems = [];
+
+galleryItems.forEach(item => {
+    // li
+    const galaryItem = document.createElement('li');
+    galaryItem.classList.add("gallery__item");
+    // a
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add("gallery__link");
+    galleryLink.setAttribute('href', item.original);
+    // img 
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = item.preview;
+    galleryImage.setAttribute('data-source', item.original);
+    // console.log(galleryImage);
+    galleryImage.alt = item.description;
+
+    galleryLink.append(galleryImage);
+    galaryItem.append(galleryLink);
+    // console.log(galaryItem);
+    // galaryRef.append(galaryItem);
+    newGalleryItems.push(galaryItem);
+});
+galaryRef.append(...newGalleryItems);
+
+
+let instance;
+
+
+galaryRef.addEventListener('click', onShowImg);
+
+function onShowImg(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== "IMG") {
+        return
+    }
+
+    // из библиотеки
+    instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600"> `);
+
+    instance.show();
+
+    document.addEventListener('keydown', onClose);
+    console.log('add EventListener'); 
+}
+
+
+
+
+function onClose(e) {
+    // console.log(e.type);
+    if(e.code !== 'Escape') {
+        return
+    }
+
+    document.removeEventListener('keydown', onClose);
+    // showEl.removeEventListener('click', onClose);
+    console.log('removed EventListener');
+    instance.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let instance;
+
+
+// galaryRef.addEventListener('click', onShowImg);
+
+// function onShowImg(event) {
+//     event.preventDefault();
+//     if(event.target.nodeName !== "IMG") {
+//         return
+//     }
+
+//     // из библиотеки
+//     instance = basicLightbox.create(`
+//         <img src="${event.target.dataset.source}" width="800" height="600"> `);
+
+//     instance.show();
+
+//     document.addEventListener('keydown', onClose);
+//     console.log('add EventListener'); 
+// }
+
+
+
+
+// function onClose(e) {
+//     // console.log(e.type);
+//     if(e.code !== 'Escape') {
+//         return
+//     }
+
+//     document.removeEventListener('keydown', onClose);
+//     // showEl.removeEventListener('click', onClose);
+//     console.log('removed EventListener');
+//     instance.close();
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const galaryRef = document.querySelector(".gallery");
+
+// === создание коллекции =====
+function createGalary(galary) {
+    const newGalleryItems = galary.map( ({preview, original, description}) => {
+    // li
+    const galaryItem = document.createElement('li');
+    galaryItem.classList.add("gallery__item");
+    // a
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add("gallery__link");
+    galleryLink.setAttribute('href', original);
+    // img 
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = preview;
+    // galleryImage.setAttribute('data-source', original);
+    // console.log(galleryImage);
+    galleryImage.alt = description;
+
+    galleryLink.append(galleryImage);
+    galaryItem.append(galleryLink);
+    // console.log(galaryItem);
+    // galaryRef.append(galaryItem);
+    // newGalleryItems.push(galaryItem);
+    return galaryItem;
+  });
+  galaryRef.append(...newGalleryItems);
+}
+
+createGalary(galleryItems);
+// === создание коллекции /конец=====
+
+
+// galaryRef.addEventListener('click', onShowImg);
+
+// function onShowImg(event) {
+//     const options = {
+//         captionsData: "alt",
+//         captionDelay: 250,
+//     };
+//     event.preventDefault();
+//     if(event.target.nodeName !== "IMG") {
+//         console.log('мимо');
+//         return
+//     }
+//     var lightbox = new SimpleLightbox('.gallery a', options);    
+//     console.log('попал');
+// }
+
+
+const options = {
+            captionsData: "alt",
+            captionDelay: 250,
+        };
+var lightbox = new SimpleLightbox('.gallery a', options);
